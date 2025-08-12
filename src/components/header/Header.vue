d4390f8c8a444851877e83f05c4cd5a2d86e7d38
Julio Zampieri <juliocezampieri@gmail.com>
2025-08-11 21:02:25 -0300

    add: standard colors and variables

---
+++ src/components/header/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <main class="container-header">
+        <header>
+            <section>
+                <img src="/img/menu.png" alt="Logo" class="logo">
+                <h1>Zampieri</h1>
+            </section>
+        </header>
+    </main>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+    .container-header{
+        padding: $mg
+    }
+
+    header{
+        color: $branco;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 3rem;
+        border-bottom: 1px solid;
+
+        section{
+            gap: 1rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            img{
+                width: 2rem;
+            }
+        }
+    }
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <main class="main">
+        <header>
+            <Header/>
+        </header>
+    </main>
+    </template>
+<script>
+import Header from '@/components/header/Header.vue';
+
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+    .main{
+        background-color: $preto;
+        height: 100vh;
+    }
+
+</style>
